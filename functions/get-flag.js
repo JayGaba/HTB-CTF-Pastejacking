b30ebd3181f208cf668e862b88d1f6fe0ff8a8f1
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2023-08-25 15:38:51 +0530

    Update get-flag.js

--- functions/get-flag.js
+++ functions/get-flag.js
@@ -1,5 +1,12 @@
 exports.handler = async (event, context) => {
-    const flag = "HTB{ }"; 
+    const flag = process.env.FLAG_SECRET;
+    if (!flag) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Flag not available" }),
+        };
+    }
+    
     return {
         statusCode: 200,
         body: JSON.stringify({ flag }),
