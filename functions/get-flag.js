414b2d8ebe82f57765f06c13deb62a3aac3f552b
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2023-08-25 15:43:04 +0530

    Update get-flag.js

--- functions/get-flag.js
+++ functions/get-flag.js
@@ -1,4 +1,15 @@
 exports.handler = async (event, context) => {
+    // Authenticate the user using Netlify Identity
+    const user = context.clientContext && context.clientContext.user;
+
+    // Check if the user is authenticated and has a specific role
+    if (!user || !user.app_metadata || user.app_metadata.roles.indexOf('admin') === -1) {
+        return {
+            statusCode: 403,
+            body: JSON.stringify({ error: "Unauthorized" }),
+        };
+    }
+
     const flag = process.env.FLAG_SECRET;
     if (!flag) {
         return {
@@ -6,7 +17,7 @@ exports.handler = async (event, context) => {
             body: JSON.stringify({ error: "Flag not available" }),
         };
     }
-    
+
     return {
         statusCode: 200,
         body: JSON.stringify({ flag }),
